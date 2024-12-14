a782567afb8aa934dfc7c24c444c96653bcdba0e
wxyz-1221 <xingyidan.1219@gmail.com>
2024-12-15 03:03:18 +0800

    implement prewitt edge detector

--- src/Operators/PrewittEdgeDetector.cs
+++ src/Operators/PrewittEdgeDetector.cs
@@ -8,7 +8,7 @@ namespace EdgeDetection.Operators
             {-1, 0, 1},
             {-1, 0, 1},
             {-1, 0, 1}
-        };
+        }; 
 
         private int[,] Gy = new int[,] {
             {-1,-1,-1},
